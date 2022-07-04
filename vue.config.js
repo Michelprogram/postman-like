16a63a7afd15e34638284b652f9d2bbd6e9db258
Michelprogram <gaurondorian@gmail.com>
2022-07-04 13:42:55 +0200

    conf vue

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
+import { defineConfig } from "@vue/cli-service";
+export default defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/postman-like/" : "/",
 });
