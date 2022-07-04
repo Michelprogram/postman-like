61ec607419c9daab034aaae324eaec42da42f298
Michelprogram <gaurondorian@gmail.com>
2022-07-04 13:49:31 +0200

    vite update config

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  base: process.env.NODE_ENV === "production" ? "/postman-like/" : "/",
 });
--- vue.config.js
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from "@vue/cli-service";
-export default defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/postman-like/" : "/",
-});
